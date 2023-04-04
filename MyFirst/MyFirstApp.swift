524940185e925d897058029efc5a5d71fdc5ec1e
Michael <mrflowers22@uwalumni.com>
2023-04-04 13:40:46 -0500

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by M F on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
